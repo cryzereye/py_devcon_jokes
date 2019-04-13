07dcd4a4c128d023ed55e26e8c2db2d919d48ac1
cryzereye <cryzereye@github.com>
2019-04-13 16:12:36 +0800

    updated views.py

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,19 @@ def index(request):
     return HttpResponse("<b>Hello World</b>")
 
 def random_joke(request):
-    # return random_joke
+    # return random_joke, may code na
     return HttpResponse("<h1>Random JOke incoming</h1>")
 
 def joke_for(request, sex):
     if sex == "male":
         return HttpResponse("<h1>joke for male</h1>")
+        # return render(request, <.html> {})
     elif sex == "female":
         return HttpResponse("<h1>joke for female</h1>")
+        # return render(request, <.html> {})
 
 def all_jokes(request):
     # return all jokes
+    jokes = []
     return HttpResponse("<h1>ALL JOKES</h1>")
+    # return render(request, '<.html>' {'jokes' : jokes})
